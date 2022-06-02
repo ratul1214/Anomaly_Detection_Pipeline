c895efafaa5738c607bd8bf4dd8ec88d439bd64c
Alam Al Saud <Alamalsaud5@gmail.com>
2022-06-02 15:43:09 +0600

    Changing IP

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def index():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.2', port=5000, debug=True)
